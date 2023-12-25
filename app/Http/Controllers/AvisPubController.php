0d4c6bc76882d88f4cf1d5ad5288490caba3f7e2
ayadiyessin <yassine.ayadi200058@gmail.com>
2023-12-25 11:24:59 +0100

    v1.2

--- app/Http/Controllers/AvisPubController.php
+++ app/Http/Controllers/AvisPubController.php
@@ -9,104 +9,12 @@ use Illuminate\Http\Request;
 
 class AvisPubController extends Controller
 {
-
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    // public function afficherCom($partId, $publId)
-    // {
-    //     // $participant = Participant::findOrFail($partId);
-    //     // $publication = Publication::findOrFail($publId);
-    //     $commentaires = AvisPub::where('participantsID', $partId)->where('publicationsID', $publId)->pluck('commentaire_pub');
-    //     return $commentaires;
-    // }
-    // public function afficherNote($partId, $publId)
-    // {
-    //     // $participant = Participant::findOrFail($partId);
-    //     // $publication = Publication::findOrFail($publId);
-    //     $note = AvisPub::where('participantsID', $partId)->where('publicationsID', $publId)->max('note_pub');
-    //     //->orderBy('champ_critere', 'desc')->first(); // tri décroissant basé sur le champ_critere
-    //     if ($note === null) {
-    //         $note = 0;
-    //     }
-    //     return $note;
-    // }
-    // public function ajoutCom(Request $request,$partId, $publId)
-    // {
-    //     $note = AvisPub::where('participantsID', $partId)
-    //     ->where('publicationsID', $publId)
-    //     ->pluck('note_note_pub')
-    //     ->max('note_pub');
-    //     if ($note === null) {
-    //         $note = 0;
-    //     }
-    //     $ajoutcoment = new AvisPub([
-    //         'publicationsID' => $request->input('publicationsID'),
-    //         'participantsID' => $request->input('participantsID'),
-    //         'note_pub' => $note,
-    //         'commentaire_pub' => $request->input('commentaire_pub'),
-    //         ]);
-    //         $ajoutcoment->save();
-    //         return response()->json($ajoutcoment,201);
-
-    // }
-    // public function ajoutnote(Request $request,$partId, $publId)
-    // {
-    //     $nouvelleNote = $request->input('note_pub');
-    //     $note = AvisPub::where('participantsID', $partId)
-    //     ->where('publicationsID', $publId)
-    //     ->pluck('note_note_pub')
-    //     ->max('note_pub');
-    //     if($note ===null){
-    //          $ajoutnote = new AvisPub([
-    //             'publicationsID' => $request->input('publicationsID'),
-    //             'participantsID' => $request->input('participantsID'),
-    //             'note_pub' => $nouvelleNote,
-    //             ]);
-    //             $ajoutnote->save();
-    //             //pdate
-    //             AvisPub::where('participantsID', $partId)
-    //             ->where('publicationsID', $publId)
-    //             ->update(['note_pub' => $nouvelleNote]);
-    //             return response()->json($ajoutnote,201);
-    //     }
-    //     else {
-    //         AvisPub::where('participantsID', $partId)
-    //             ->where('publicationsID', $publId)
-    //             ->update(['note_pub' => $nouvelleNote]);
-
-    //         return "Toutes les lignes mises à jour avec la nouvelle note : $nouvelleNote";
-    //     }
-
-
-
-    // }
-
-
-
-    // /**
-    //  * Update the specified resource in storage.
-    //  */
-    // public function update(Request $request, AvisPub $avisPub)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Remove the specified resource from storage.
-    //  */
-    // public function destroy(AvisPub $avisPub)
-    // {
-    //     //
-    // }
-
     public function index($partId, $publId)
     {
         $commentaires = AvisPub::where('participantsID', $partId)->where('publicationsID', $publId)->get();//->pluck('commentaire_pub')
         return $commentaires;
     }
-    // pour la liste des comm par publication 
+    // pour la liste des comm par publication
     public function listeCommentaires($publId)
     {
         $commentaires = AvisPub::where('publicationsID', $publId)->orderBy('updated_at', 'asc')->get();//->pluck('commentaire_Event')
